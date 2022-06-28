39431cfd4a8c91370c8c0a0aa3c325f8d7903875
4t145 <angler@hust.edu.cn>
2022-06-29 00:05:25 +0800

    fix deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,18 +1,26 @@
 LOG_PATH="/var/log/vrmonitor"
-ROOT_DIR=$1
+ROOT_DIR=$PWD
 readonly LOG_PATH
 readonly ROOT_DIR
 
+function check_dir()
+{
+    if [ ! -d $1 ]
+    then mkdir $1
+    fi 
+}
 
-mkdir ${LOG_PATH}
+check_dir ${LOG_PATH};
+check_dir ${ROOT_DIR}/bin;
 
 # build server
 cd ${ROOT_DIR}/server/openapi/
-echo '[build server]'
+echo "[build server]"
 cargo build --release
+cd ${ROOT_DIR}
 
-mkdir ${ROOT_DIR}/bin
-cp ${ROOT_DIR}/server/openapi/target/openapi ${ROOT_DIR}/bin
+cp ${ROOT_DIR}/server/openapi/target/release/openapi ${ROOT_DIR}/bin
 
-echo '[launch openapi]'
+echo "[launch openapi]"
+echo "log file: ${LOG_PATH}/openapi.log"
 nohup ${ROOT_DIR}/bin/openapi ${ROOT_DIR}/config/openapi/config.toml > ${LOG_PATH}/openapi.log &
